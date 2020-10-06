1afb2d3a2d30e151397c10aee5614efab6e03a54
IMRYOUNG <limryoung98@naver.com>
2020-10-06 23:49:28 +0900

    Initial commit

---
+++ eatgo-common/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,32 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class RestaurantTests {
+
+    @Test
+    public void creation(){
+        Restaurant restaurant = Restaurant.builder()
+                .id(1004L)
+                .name("Bob zip")
+                .address("Seoul")
+                .build();
+
+        assertThat(restaurant.getName(),is("Bob zip"));
+        assertThat(restaurant.getAddress(),is("Seoul"));
+        assertThat(restaurant.getId(), is(1004L));
+    }
+
+    @Test
+    public void information(){
+        Restaurant restaurant = Restaurant.builder()
+                .id(1004L)
+                .name("Bob zip")
+                .address("Seoul")
+                .build();
+        assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
+    }
+}
